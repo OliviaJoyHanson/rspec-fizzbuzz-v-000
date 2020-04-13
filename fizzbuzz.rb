2b25458e49bbfdcc12cf85507b73dd2583d3471a
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-13 14:30:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 15 == 0
-  "FizzBuzz"
-  elsif int % 5 == 0
-    "Buzz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   elsif int % 3 == 0
     "Fizz"
-  else int % 4 == 0
+  elsif int % 5 == 0
+    "Buzz"
+  else
     nil
   end
 end
